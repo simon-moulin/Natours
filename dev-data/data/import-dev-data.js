adfa61cae5e090bfe682c0df34d27585a280eb88
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-02-16 18:44:45 +0100

    :sparkles: Add files

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const Tour = require('./../../model/tourModel');
+
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE;
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('DB connection successful'));
+
+// READ JSON
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT INTO DB
+
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded!');
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
+};
+
+// DELETE ALL DATA FROM COLLECTION
+
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted');
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
+};
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}
+
+console.log(process.argv);
