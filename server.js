3b779aa93b39f5548378c32b5ecfe2e62f404471
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-03-03 17:34:04 +0100

    Error handling ok

--- server.js
+++ server.js
@@ -2,6 +2,17 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('unhandledRejection', err => {
+    console.log(err);
+    console.log('UNHANDLED REJECTION');
+    process.exit(1);
+});
+
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT REJECTION');
+    process.exit(1);
+});
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
