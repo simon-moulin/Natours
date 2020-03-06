139ea0ff0cdef66bc9422ecdb0495a7fd122d17c
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-03-06 17:42:22 +0100

    Protecting routes ok

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,26 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.signup = catchAsync(async (req, res, next) => {
-    //const newUser = await User.create(req.body);
-
-    const newUser = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
-    });
-
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN
     });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create(req.body);
+
+    // const newUser = await User.create({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    //     passwordConfirm: req.body.passwordConfirm
+    // });
+
+    const token = signToken(newUser._id);
 
     res.status(201).json({
         status: 'success',
@@ -23,4 +29,71 @@ exports.signup = catchAsync(async (req, res, next) => {
             user: newUser
         }
     });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    //Check if user and email exists
+
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password', 400));
+    }
+
+    //Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401));
+    }
+
+    //Send token to user
+
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token
+    });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+    //1) Getting token and check of it's there
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+        return next(
+            new AppError(
+                'You are not logged in! Please login to get access.',
+                401
+            )
+        );
+    }
+    //2) Verif token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    //3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(
+            new AppError(
+                'The user belonging to this token does no longer exist.',
+                401
+            )
+        );
+    }
+
+    //4) Check if user changed password after JWT was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(new AppError('User recently changed password!', 401));
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+
+    next();
 });
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,6 +11,12 @@ const handleDuplicateFieldsDB = err => {
     return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+    new AppError('Invalid token! Please log in again', 401);
+
+const handleJWTExpiredError = () =>
+    new AppError('Your token has expired! Please log in again', 401);
+
 const handleValidationErrorDB = err => {
     const errors = Object.values(err.errors).map(el => el.message);
     const message = `Invalid input data. ${errors.join('. ')}`;
@@ -56,6 +62,9 @@ module.exports = (err, req, res, next) => {
         if (error.name === 'ValidationError') {
             error = handleValidationErrorDB(error);
         }
+        if (error.name === 'JsonWebTokenError') error = handleJWTError();
+        if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
         sendErrorProd(error, res);
     }
 };
