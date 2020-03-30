615edebb327bd83c595bd137a3b6c0deac7dd095
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-03-30 15:54:46 +0200

    Auth terminé

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,9 +11,22 @@ const signToken = id => {
         expiresIn: process.env.JWT_EXPIRES_IN
     });
 };
+const cookieOptions = {
+    expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+};
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+
+    if (process.env.NODE_ENV === 'production') {
+        cookieOptions.secure = true;
+    }
+
+    user.password = undefined;
+    res.cookie('jwt', token, cookieOptions);
 
     res.status(statusCode).json({
         status: 'success',
