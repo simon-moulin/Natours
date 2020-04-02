6ad4c8ac80f79156b07df2c3b81fc28559c0b819
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-04-02 15:25:04 +0200

    Factory for CRUD

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,31 +1,16 @@
 const Review = require('../model/reviewModel');
-const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-    let filter = {};
-    if (req.params.tourId) filter = { tour: req.params.tourId };
+exports.getAllReviews = factory.getAll(Review);
 
-    const reviews = await Review.find(filter);
-
-    res.status(200).json({
-        status: 'success',
-        resulats: reviews.length,
-        data: {
-            reviews
-        }
-    });
-});
-
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
     // Allow nested routes
     if (!req.body.tour) req.body.tour = req.params.tourId;
     if (!req.body.user) req.body.user = req.user.id;
-
-    const newReview = await Review.create(req.body);
-    res.status(201).json({
-        status: 'success',
-        data: {
-            review: newReview
-        }
-    });
-});
+    next();
+};
+exports.createReview = factory.createOne(Review);
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../model/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
@@ -11,19 +12,6 @@ const filterObj = (obj, ...allowedFields) => {
     });
     return newObj;
 };
-
-exports.getAllUsers = catchAsync(async (req, res) => {
-    const users = await User.find();
-
-    //SEND RESPONSE
-    res.status(200).json({
-        status: 'success',
-        results: users.length,
-        data: {
-            users
-        }
-    });
-});
 
 exports.updateMe = catchAsync(async (req, res, next) => {
     // 1) Create error if user POSTs password data
@@ -62,29 +50,8 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    });
-};
-
-exports.deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    });
-};
-exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    });
-};
-
-exports.getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    });
-};
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
+// DO NOT update passwords with this
+exports.updateUser = factory.updateOne(User);
+exports.deleteUser = factory.deleteOne(User);
