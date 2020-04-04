241b83b1b23afa994d03194e275e1e892fce939a
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-04-04 17:42:37 +0200

    Login frontend added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookierParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -37,6 +38,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookierParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -66,7 +68,7 @@ if (process.env.NODE_ENV === 'development') {
 // Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
-    //console.log(req.headers);
+    console.log(req.cookies);
     next();
 });
 
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,48 +1,44 @@
 /* eslint-disable */
-console.log('hello from the client side :D');
+export const displayMap = locations => {
+    mapboxgl.accessToken =
+        'pk.eyJ1IjoiYXBva2FsaXBzMjgiLCJhIjoiY2s4bG1iOGV5MDNoMDNubjg0dzltcGg5NCJ9.1K-90VQN3PQUspttd2jviA';
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/apokalips28/ck8lmjm670t6s1iod8xaopwen',
+        scrollZoom: false
+        // center: [-118.113491, 34.111745],
+        // zoom: 4,
+        // interactive:false
+    });
 
-console.log(locations);
+    const bounds = new mapboxgl.LngLatBounds();
 
-mapboxgl.accessToken =
-    'pk.eyJ1IjoiYXBva2FsaXBzMjgiLCJhIjoiY2s4bG1iOGV5MDNoMDNubjg0dzltcGg5NCJ9.1K-90VQN3PQUspttd2jviA';
+    locations.forEach(loc => {
+        // Add marker
+        const el = document.createElement('div');
+        el.className = 'marker';
 
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/apokalips28/ck8lmjm670t6s1iod8xaopwen',
-    scrollZoom: false
-    // center: [-118.113491, 34.111745],
-    // zoom: 4,
-    // interactive:false
-});
-
-const bounds = new mapboxgl.LngLatBounds();
-
-locations.forEach(loc => {
-    // Add marker
-    const el = document.createElement('div');
-    el.className = 'marker';
-
-    new mapboxgl.Marker({
-        element: el,
-        anchor: 'bottom'
-    })
-        .setLngLat(loc.coordinates)
-        .addTo(map);
-    new mapboxgl.Popup({
-        offset: 30
-    })
-        .setLngLat(loc.coordinates)
-        .setHTML(`<p>Day ${loc.day} : ${loc.description}</p>`)
-        .addTo(map);
-    bounds.extend(loc.coordinates);
-});
-map.fitBounds(bounds, {
-    padding: {
-        top: 200,
-        bottom: 100,
-        left: 100,
-        right: 100
-    }
-});
+        new mapboxgl.Marker({
+            element: el,
+            anchor: 'bottom'
+        })
+            .setLngLat(loc.coordinates)
+            .addTo(map);
+        new mapboxgl.Popup({
+            offset: 30
+        })
+            .setLngLat(loc.coordinates)
+            .setHTML(`<p>Day ${loc.day} : ${loc.description}</p>`)
+            .addTo(map);
+        bounds.extend(loc.coordinates);
+    });
+    map.fitBounds(bounds, {
+        padding: {
+            top: 200,
+            bottom: 100,
+            left: 100,
+            right: 100
+        }
+    });
+};
