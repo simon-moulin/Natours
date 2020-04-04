4698dc65da75581a713afc9972596ad4d4df5839
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-04-04 14:38:58 +0200

    Tour view

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,8 +14,15 @@ exports.getOverview = catchAsync(async (req, res) => {
     });
 });
 
-exports.getTour = (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res) => {
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
     });
-};
+    // 2) Create pug template
+
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker Tour',
+        tour
+    });
+});
