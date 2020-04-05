3eb485f81be714b52526e031200ecfd9ffc3c02a
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-04-05 17:01:56 +0200

    fixing stuff

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,6 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
-
 import { updateSettings } from './updateSettings';
 
 // DOM ELEMENTS
@@ -10,44 +9,47 @@ const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
-const userPasswordForm = form-user-settings
-
-// VALUES
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // DELEGATION
 if (mapBox) {
-    const locations = JSON.parse(mapBox.dataset.locations);
-    displayMap(locations);
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
 }
 
-if (loginForm) {
-    loginForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        login(email, password);
-    });
-}
+if (loginForm)
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
 
-if (logOutBtn) {
-    logOutBtn.addEventListener('click', logout);
-}
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if (userDataForm) {
-    userDataForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        updateSettings({ name, email }, 'data');
-    });
-}
+if (userDataForm)
+  userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
 
-if (userPasswordForm) {
-    userPasswordForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const passwordCurrent = document.getElementById('password-current').value;
-        const password = document.getElementById('password').value;
-        const passwordConfirm = document.getElementById('passwordConfirm').value;
-        updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
-    });
-}
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
