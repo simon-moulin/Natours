1a8d5d307e6432eecc85a0d1dfc62353f1068725
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-04-07 16:21:09 +0200

    Mise a jour

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8939,7 +8939,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50384" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57761" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
